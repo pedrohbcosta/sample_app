6a4194cb1120e7072f606e3433a43e755fd426fa
Pedro <pedro@yellowberry.com.br>
2023-03-20 19:25:41 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample app"
+      redirect_to @user
+      #redirect_to user_url(@user)
+      # Handle the successful save.
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                    :password_confirmation)
+    end
 end
