1e266950d4d41f36eb87fff7d1527ae125b65659
Pedro Henrique Batista Costa <pedrobc.engsoft@gmail.com>
2023-04-03 23:38:38 -0300

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true)
+            email: "example@railstutorial.org",
+            password:              "foobar",
+            password_confirmation: "foobar",
+            admin:     true,
+            activated: true,
+            activated_at: Time.zone.now)
 
 # Generate a bunch of additional users.
 99.times do |n|
@@ -11,7 +13,9 @@ User.create!(name:  "Example User",
   password = "password"
 
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -7,7 +7,7 @@ class SessionsHelperTest < ActionView::TestCase
     remember(@user)
   end
 
-  test "current_user returns right user when session is nill" do
+  test "current_user returns right user when session is nil" do
     assert_equal @user, current_user
     assert is_logged_in?
   end
